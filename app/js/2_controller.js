3310772886c0c2aa0ee9de994b9b6026521f2eff
Suranart Niamcome <suranartnc@gmail.com>
2015-03-27 14:06:22 +0700

    reset the project

--- app/js/2_controller.js
+++
@@ -1,9 +0,0 @@
-var myControllers = angular.module('myControllers', []);
-
-myControllers.controller('MainCtrl', ['$scope', function($scope){
-	$scope.page = '1';
-}]);
-
-myControllers.controller('PageCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
-	$scope.page = $routeParams.page;
-}]);
